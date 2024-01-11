2b624122326ff533a6d96a89157311d3534c7f35
Parfent1ev <143738002+Parfent1ev@users.noreply.github.com>
2024-01-11 14:37:00 +0000

    commit

---
+++ js/task-4.js
@@ -0,0 +1,52 @@
+const getTotalBalanceByGender = (users, gender) => {
+  const filteredArr = users.filter((user) => {
+    return user.gender === gender;
+  });
+  const totalSum = filteredArr.reduce((acc, user) => {
+    acc += user.balance;
+    return acc;
+  }, 0);
+  return totalSum;
+};
+
+const allUsers = [
+  {
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811,
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821,
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793,
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278,
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951,
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498,
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764,
+  },
+];
+
+console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+
+console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
